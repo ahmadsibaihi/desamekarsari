1d3e80445862c62c6a321f7c29323934398bfca9
Ahmad Syibaihi <ahmadsyibaihi@gmail.com>
2025-02-21 23:54:32 +0700

    update news

--- assets/js/main.js
+++ assets/js/main.js
@@ -193,6 +193,18 @@
     }
   });
 
+  document.addEventListener("DOMContentLoaded", function () {
+    const img = document.querySelector(".custom-img");
+
+    document.addEventListener("mousemove", function (e) {
+        const x = (e.clientX / window.innerWidth - 0.5) * 20; /* Gerakan horizontal */
+        const y = (e.clientY / window.innerHeight - 0.5) * 20; /* Gerakan vertikal */
+        
+        img.style.transform = `translate(${x}px, ${y}px)`;
+    });
+});
+
+
   /**
    * Navmenu Scrollspy
    */
